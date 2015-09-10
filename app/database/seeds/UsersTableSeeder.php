96d7ab421394375f67b4283020c2bb6b42a8053d
dgcollier <david@gcollier.me>
2015-09-10 15:39:11 -0500

    added migrations and seeders

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder {
+
+	public function run()
+	{
+		User::create([
+
+			'username'   => $_ENV['USER_FIRSTNAME'],
+			'first_name' => $_ENV['USER_LASTNAME'],
+			'last_name'  => $_ENV['USER_EMAIL'],
+			'email'      => $_ENV['USER_USERNAME'],
+			'password'   => $_ENV['USER_PASSWORD'],
+		]);
+
+		$faker = Faker::create('en_EN');
+
+		foreach(range(1, 3) as $index)
+		{
+			User::create([
+				
+				'username'   => $faker->userName,
+				'first_name' => $faker->firstName,
+				'last_name'  => $faker->lastName,
+				'email'      => $faker->freeEmail,
+				'password'   => $faker->password,
+			]);
+		}
+	}
+
+}
