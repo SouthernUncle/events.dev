b8b9b8de047e6c016e0fc219321a9b0b69c67f3c
j-womack <womack.joshua@gmail.com>
2015-09-11 11:19:09 -0500

    new photos

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -40,7 +40,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">noisefights.com</a>
+                <a class="navbar-brand" href="/">Concert Lister</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
