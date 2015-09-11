4433b153baed614508e675b73064cef76930e7f1
dgcollier <david@gcollier.me>
2015-09-11 11:19:45 -0500

    updated index with table and some data displayed. in progress.

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,7 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+
 // GET
 // HOMEPAGE
 Route::get('/', 'CalendarEventsController@index');
--- app/views/calendarEvents/index.blade.php
+++ app/views/calendarEvents/index.blade.php
@@ -1,5 +1,51 @@
 @extends('layouts.master')
 
+@section('title')
+	Events Index
+@stop
+
+@section('style')
+
+@stop
+
+@section('heading')
+	Awesome Events
+@stop
+
+@section('subheading')
+	blah blah blah
+@stop
+
+@section('image_url')
+''
+@stop
+
 @section('content')
-/vagrant/sites/events.dev/app/views/calendarEvents/index.blade.php
+	<table>
+		<thead>
+			<tr>
+				<th>Event Name</th>
+				<th>Location</th>
+				<th>Start</th>
+				<th>End</th>
+				<th>City</th>
+
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($calendarEvents as $ce)
+			<tr>
+				<td>{{ $ce->title }}</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			@endforeach
+		</tbody>
+
+	</table>
+@stop
+
+@section('js')
+
 @stop
