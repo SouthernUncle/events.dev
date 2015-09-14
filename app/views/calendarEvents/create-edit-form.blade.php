eb1e42143b8170ae9699967a36110b005b9efe03
j-womack <womack.joshua@gmail.com>
2015-09-14 11:31:50 -0500

    date/time picker

--- app/views/calendarEvents/create-edit-form.blade.php
+++ app/views/calendarEvents/create-edit-form.blade.php
@@ -1,17 +1,25 @@
 {{ Form::label('title', 'Title') }}
 {{ Form::text('title', null, ['class' => 'form-control']) }}
 
-<label for="body">Body</label>
+<label for="description">Description</label>
         
 <div class="wmd-panel">
     <div id="wmd-button-bar"></div>
-    <textarea class="wmd-input form-control" name="body" cols="50" rows="10" id="wmd-input"></textarea>
+    <textarea class="wmd-input form-control" name="description" cols="50" rows="10" id="wmd-input"></textarea>
 </div>
 <label>Preview:</label>
 <div id="wmd-preview" class="wmd-panel wmd-preview"></div>
 
-{{ Form::label('tags', 'Tags') }}
-{{ Form::text('tags', null, ['class' => 'form-control']) }}
+{{ Form::label('price', 'Price ($)') }}
+{{ Form::number('price', null, ['class' => 'form-control']) }}
+
+    <label>Date and Time</label>
+<div>
+    <input id="datetimepicker" type="text" >
+</div>
+
+{{ Form::label('location', 'Location') }}
+{{ Form::text('location', null, ['class' => 'form-control']) }}
 
 {{ Form::label('file','File',array('id'=>'','class'=>'')) }}
 {{ Form::file('file','',array('id'=>'','class'=>'')) }}
